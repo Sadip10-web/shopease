ae34a76282ed4e26d3d72d933b74d2355212938a
Sadip10-web <sadipthapa630@gmail.com>
2026-06-22 14:45:28 +0545

    test

---
+++ lib/views/a.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" Git Example"),
+        centerTitle: true,
+      ),
+
+      body: Column(
+        children: [
+          Text("Sadip"),
+          Text("Mahesh")
+        ],
+      ),
+    );
+  }
+}
