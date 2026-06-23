443d588a9a51807c8382c42e07f1f47603aaa7a4
Raj-Chhetri <kecy3929@gmail.com>
2026-06-23 19:24:42 +0545

    Implemented payment screen UI

--- lib/widgets/Screentitle.dart
+++ lib/widgets/Screentitle.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 class Screen_title extends StatelessWidget {
-  String text;
+  final String text;
   
    Screen_title({
     super.key,
