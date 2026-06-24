e9c937d569518cfc2a2ed780b5e6f4db5d5c51d4
Sadip10-web <sadipthapa630@gmail.com>
2026-06-24 15:55:15 +0545

    Change password screen and new passwordfield widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shopease/views/changepassword_screen.dart';
 import 'package:shopease/views/login_view.dart';
 import 'package:shopease/views/register_view.dart';
 import 'package:shopease/views/Verifyotp.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.white),
       ),
-      home: Verifyotp(),
+      home: ChangepasswordScreen(),
     );
   }
 }
