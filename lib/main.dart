17ffde98a1bc1b721a7d710167cf2e98200bcffc
pankaj-mandal27 <ronishraj27@gmail.com>
2026-06-25 03:28:20 +0545

    google_fonts added in pubsec.yamal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+// import 'package:shopease/views/login_view.dart';
+// import 'package:shopease/views/register_view.dart';
+// import 'package:shopease/views/Verifyotp.dart';
 import 'package:shopease/views/homescreen.dart';
 
 void main() {
@@ -11,7 +15,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.white),
