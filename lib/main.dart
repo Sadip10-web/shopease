1750dcc9e96c0f462b51ca828229232c3eaeb794
Kheral77 <117247831+Kheral77@users.noreply.github.com>
2026-06-25 17:41:31 +0545

    Setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopease/views/homescreen.dart';
+import 'package:shopease/views/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,16 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.white),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
+
+      // Choose ONLY ONE home screen
       home: LoginView(),
-      home: ChangepasswordScreen(),
+
+      // home: ChangepasswordScreen(),
     );
   }
 }
