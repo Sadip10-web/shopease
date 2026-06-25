2abaa3afd2c3b31febd0939258c37aba0b7a90eb
pankaj-mandal27 <ronishraj27@gmail.com>
2026-06-25 15:43:46 +0545

    abhishek

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.white),
       ),
-      home:Splashscreen(),
+      home:HomeScreen(),
     );
   }
 }
