8ed0b912b517bbffde67f3a0f09da3f99807ea3e
Maheshrawat02 <maheshrawat9848@gmail.com>
2026-06-25 12:02:30 +0545

    Login and register screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopease/views/homescreen.dart';
+import 'package:shopease/views/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.white),
       ),
-      home: ChangepasswordScreen(),
+      home: LoginView(),
     );
   }
 }
