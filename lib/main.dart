31602736a24efe89a1cc4770d54dade012210468
Maheshrawat02 <maheshrawat9848@gmail.com>
2026-06-27 13:54:38 +0545

    passwordfield modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shopease/views/Splashscreen.dart';
+import 'package:shopease/views/Verifyotp.dart';
 import 'package:shopease/views/edit_profile_screen.dart';
+import 'package:shopease/views/forgot_password_view.dart';
 import 'package:shopease/views/homescreen.dart';
+import 'package:shopease/views/login_view.dart';
 import 'package:shopease/views/order_history_view.dart';
 import 'package:shopease/views/product_detail.dart';
+import 'package:shopease/views/terms_condition.dart';
+import 'package:shopease/views/wishlist_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.white),
       ),
-      home: EditProfileScreen(),
+      home: LoginView(),
     );
   }
 }
