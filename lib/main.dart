61bf1972804ac48ae3f2ca5a23bd71e9317946a1
Maheshrawat02 <maheshrawat9848@gmail.com>
2026-06-30 15:13:39 +0545

    login upadated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shopease/views/Verifyotp.dart';
+import 'package:shopease/views/cartScrenn_view.dart';
+import 'package:shopease/views/changepassword_screen.dart';
+import 'package:shopease/views/forgot_password_view.dart';
+import 'package:shopease/views/homescreen.dart';
+import 'package:shopease/views/login_view.dart';
+import 'package:shopease/views/order_success.dart';
+import 'package:shopease/views/payment_screen.dart';
+import 'package:shopease/views/register_view.dart';
+import 'package:shopease/views/wishlist_view.dart';
+import 'views/login_view.dart';
 import 'package:shopease/views/Splashscreen.dart';
 import 'package:shopease/views/cartScrenn_view.dart';
 import 'package:shopease/views/category_view.dart';
