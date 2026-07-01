d78131701ea9280a57fd54fd8bb705a4175974a6
Sadip10-web <sadipthapa630@gmail.com>
2026-07-01 13:46:33 +0545

    otp field changes

--- lib/views/changepassword_screen.dart
+++ lib/views/changepassword_screen.dart
@@ -12,52 +12,54 @@ class ChangepasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(22),
-        child: Column(
-          children: [
-        ScreenTitle(text: "CHANGE PASSWORD"),
-         PasswordFieldWidget(text: 'New Password', hintText: 'Enter new Password',),
-         PasswordFieldWidget(text: "Confirm Password ", hintText: "Re-enter new Password"),
-         Row(
-      children: [
-        Expanded(
-          child: FilledButton.icon(
-            style: FilledButton.styleFrom(
-              minimumSize: const Size(double.infinity, 60),
-              backgroundColor:  Color(0xFF6D28FF),
-              side: BorderSide(color: Colors.grey.shade300),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadiusGeometry.circular(15),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(22),
+          child: Column(
+            children: [
+          ScreenTitle(text: "CHANGE PASSWORD"),
+           PasswordFieldWidget(text: 'New Password', hintText: 'Enter new Password',),
+           PasswordFieldWidget(text: "Confirm Password ", hintText: "Re-enter new Password"),
+           Row(
+        children: [
+          Expanded(
+            child: FilledButton.icon(
+              style: FilledButton.styleFrom(
+                minimumSize: const Size(double.infinity, 60),
+                backgroundColor:  Color(0xFF6D28FF),
+                side: BorderSide(color: Colors.grey.shade300),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(15),
+                ),
+                elevation: 0,
+                
               ),
-              elevation: 0,
+              onPressed: (){
+                Get.snackbar(
+                  "Sucess",
+                   "Password changed sucessfully",
+                   duration: const Duration(seconds: 2)
+                   );
+                
+                Future.delayed(Duration(seconds: 2),(){
+                  Get.offAll(()=>LoginView());
+                });
+              },
               
-            ),
-            onPressed: (){
-              Get.snackbar(
-                "Sucess",
-                 "Password changed sucessfully",
-                 duration: const Duration(seconds: 2)
-                 );
-              
-              Future.delayed(Duration(seconds: 2),(){
-                Get.offAll(()=>LoginView());
-              });
-            },
-            
-            label: Text(
-              "Change Password",
-              style: TextStyle(
-                fontSize: 21,
-                fontWeight: FontWeight.bold,
-                color:Colors.white,
+              label: Text(
+                "Change Password",
+                style: TextStyle(
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
+                  color:Colors.white,
+                ),
               ),
             ),
           ),
-        ),
-      ],
-    )
-          ],
+        ],
+            )
+            ],
+          ),
         ),
       ),
     );
