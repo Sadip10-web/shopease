a0ffef4ea2113a7a4b75e74b32b2c335c66b58f4
Kheral77 <117247831+Kheral77@users.noreply.github.com>
2026-07-02 12:12:00 +0545

    settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shopease/views/Splashscreen.dart';
-import 'package:shopease/views/edit_profile_screen.dart';
+import 'package:shopease/controller/app_controller.dart';
+import 'package:shopease/settings_page.dart';
+import 'package:shopease/translation/app_translation.dart';
 
 void main() {
+  final appController = AppController();
+  Get.put(appController);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.white)),
-      home: Splashscreen(),
+    final controller = Get.find<AppController>();
+
+    return Obx(
+      () => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+
+        translations: AppTranslation(),
+
+        locale: controller.language.value == 'Nepali'
+            ? const Locale('ne', 'NP')
+            : const Locale('en', 'US'),
+
+        fallbackLocale: const Locale('en', 'US'),
+
+        theme: ThemeData(
+          useMaterial3: false,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+
+        darkTheme: ThemeData(
+          useMaterial3: false,
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: const Color(0xFF121212),
+        ),
+
+        themeMode: controller.isDark.value ? ThemeMode.dark : ThemeMode.light,
+
+        home: const SettingsPage(),
+      ),
     );
   }
 }
